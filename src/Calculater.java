cc67ef21fad81ddc0920e0a19df430ee2d0a5da7
HENRIQUE BRENDON <henriquebrendon26@gmail.com>
2022-12-15 22:35:43 -0300

    Classe

---
+++ src/Calculater.java
@@ -0,0 +1,60 @@
+
+public class Calculater {
+	private Integer n1;
+	private Integer n2;
+	private Integer r;
+// --------------------------
+	private Double r1;
+	private Double r2;
+	private Double s;
+
+//      Integer
+public void setN1(Integer N1) {
+	n1 = N1;
+}
+public Integer getN1() {
+	return n1;
+}
+
+public void setN2(Integer N2) {
+	n2 = N2;
+}
+public Integer getN2() {
+	return n2;
+}
+
+public void setR(Integer R) {
+	r = R;
+}
+public Integer getR() {
+	return r;
+}
+// ---------------------------------------
+
+//		Double]
+
+public void setR1(Double R1) {
+	r1 = R1;
+}
+public Double getR1() {
+	return r1;
+}
+
+public void setR2(Double R2) {
+	r2 = R2;
+}
+public Double getR2() {
+	return r2;
+}
+
+public void setS(Double S) {
+	s = S;
+}
+public Double getS() {
+	return s;
+}
+// ---
+	
+	
+
+}
