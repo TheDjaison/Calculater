190e02db9002310f3cdb931fc6a39edc8980cf18
HENRIQUE BRENDON <henriquebrendon26@gmail.com>
2022-12-15 22:21:41 -0300

    Importando blibliotecas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Step 1: import libraires
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	sc.close();
 
 	}
 
